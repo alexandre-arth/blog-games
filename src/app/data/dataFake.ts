8ea3c5cfd409a70afd6bc2383f5845ffbd7a32f1
alexandre-arth <arthurmalexandre@hotmail.com>
2024-01-03 11:14:08 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "GTA: qual jogo teve a melhor campanha de todas?",
+    "description": "A história de Grand Theft Auto pode surpreender os jogadores",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
